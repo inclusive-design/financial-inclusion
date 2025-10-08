75c3dd2625b670952e57892359272af7b0522687
Ned Zimmerman <ned@bight.dev>
2025-10-08 16:49:27 -0300

    fix: improvements for DocRaptor

--- eleventy.config.js
+++ eleventy.config.js
@@ -200,8 +200,7 @@ export default function eleventy(eleventyConfig) {
           // open('_site/assets/guidebook-for-financial-inclusion.pdf');
         });
       }
-
-    // }
+    }
   );
 
   return {
