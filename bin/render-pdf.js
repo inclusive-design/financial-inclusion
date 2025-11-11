9f4bf47d15ccc72829730d7e8e15499cc6413a96
Ned Zimmerman <ned@bight.dev>
2025-11-11 15:07:02 -0400

    feat: handle both languages for PDF rendering

--- bin/render-pdf.js
+++ bin/render-pdf.js
@@ -4,28 +4,35 @@ import { exec } from 'node:child_process';
 
 const user_credentials = process.env.DOCRAPTOR_API_KEY || 'YOUR_API_KEY_HERE';
 
-const config = {
-  url: "https://api.docraptor.com/docs",
-  method: "post",
-  responseType: "arraybuffer",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  data: {
-    user_credentials,
-    doc: {
-      test: user_credentials === 'YOUR_API_KEY_HERE' || false,
-      document_type: "pdf",
-      document_url: "https://financial-inclusion.pages.dev/en/export/index.html",
-      pipeline: 11,
-      prince_options: {
-        media: "print",
-        baseurl: "https://financial-inclusion.pages.dev",
-        profile: "PDF/UA-1"
+const languages = {
+  "en": "guidebook-for-financial-inclusion",
+  "fr": "guide-pour-l-inclusion-financiere"
+};
+
+const docraptorConfig = (lang) => {
+  return {
+    url: "https://api.docraptor.com/docs",
+    method: "post",
+    responseType: "arraybuffer",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    data: {
+      user_credentials,
+      doc: {
+        test: user_credentials === 'YOUR_API_KEY_HERE' || false,
+        document_type: "pdf",
+        document_url: `https://financial-inclusion.inclusivedesign.ca/${lang}/export/index.html`,
+        pipeline: 11,
+        prince_options: {
+          media: "print",
+          baseurl: "https://financial-inclusion.inclusivedesign.ca/",
+          profile: "PDF/UA-1"
+        }
       }
     }
-  }
-};
+  };
+}
 
 function princeVersion() {
   return new Promise((resolve) => {
@@ -38,23 +45,29 @@ function princeVersion() {
 const prince = await princeVersion();
 
 if (prince.includes('Prince 16')) {
-  console.log('Rendering PDF using Prince binary...');
-  exec(`prince --pdf-profile='PDF/UA-1' http://localhost:8080/en/export/ -o src/assets/downloads/guidebook-for-financial-inclusion.pdf`, (_error, stdout, _stderr) => {
-    console.log("Saved guidebook-for-financial-inclusion.pdf.");
-  });
+  console.log('Rendering PDFs using Prince binary...');
+
+  for (const [lang, filename] of Object.entries(languages)) {
+    exec(`prince --pdf-profile='PDF/UA-1' http://localhost:8080/${lang}/export/ -o src/assets/downloads/${filename}.pdf`, (_error, stdout, _stderr) => {
+      console.log(`Saved ${filename}.pdf.`);
+    });
+  }
 } else {
-  console.log('Rendering PDF using DocRaptor API...');
-  axios(config)
+  console.log('Rendering PDFs using DocRaptor API...');
+
+  for (const [lang, filename] of Object.entries(languages)) {
+    axios(docraptorConfig(lang))
     .then(function (response) {
-      fs.writeFile("./src/assets/downloads/guidebook-for-financial-inclusion.pdf", response.data, "binary", function (
+      fs.writeFile(`./src/assets/downloads/${filename}.pdf`, response.data, "binary", function (
         writeErr
       ) {
         if (writeErr) throw writeErr;
-        console.log("Saved guidebook-for-financial-inclusion.pdf.");
+        console.log(`Saved ${filename}.pdf.`);
       });
     })
     .catch(function (error) {
       var decoder = new TextDecoder("utf-8");
       console.error(decoder.decode(error.response.data));
     });
+  }
 }
