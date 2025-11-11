f06a095b36fbe8345a97cccf6644c3df08e7d415
Ned Zimmerman <ned@bight.dev>
2025-11-11 15:20:08 -0400

    ci: handle test vs production API credentials (#143)
    * ci: handle test vs production API credentials
    
    * ci: reference variable better
    
    * ci: check out repository
    
    * ci: try saving to root
    
    * ci: debug
    
    * ci: use default pipeline
    
    * ci: verbose curl
    
    * ci: adjust pipeline
    
    * ci: use Node script
    
    * ci: use Node script
    
    * ci: remove rm
    
    * ci: adjust workflow
    
    * ci: debug
    
    * ci: debug
    
    * ci: debug
    
    * ci: remove debugging
    
    * fix: adjust path
    
    * ci: adjust path
    
    * fix: restore path
    
    * feat: handle both languages for PDF rendering
    
    * ci: upload both PDFs

---
+++ bin/render-pdf.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import fs from 'node:fs';
+import { exec } from 'node:child_process';
+
+const user_credentials = process.env.DOCRAPTOR_API_KEY || 'YOUR_API_KEY_HERE';
+
+const languages = {
+  "en": "guidebook-for-financial-inclusion",
+  "fr": "guide-pour-l-inclusion-financiere"
+};
+
+const docraptorConfig = (lang) => {
+  return {
+    url: "https://api.docraptor.com/docs",
+    method: "post",
+    responseType: "arraybuffer",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    data: {
+      user_credentials,
+      doc: {
+        test: user_credentials === 'YOUR_API_KEY_HERE' || false,
+        document_type: "pdf",
+        document_url: `https://financial-inclusion.inclusivedesign.ca/${lang}/export/index.html`,
+        pipeline: 11,
+        prince_options: {
+          media: "print",
+          baseurl: "https://financial-inclusion.inclusivedesign.ca/",
+          profile: "PDF/UA-1"
+        }
+      }
+    }
+  };
+}
+
+function princeVersion() {
+  return new Promise((resolve) => {
+    exec('prince --version', (_error, stdout, _stderr) => {
+      resolve(stdout);
+    });
+  });
+}
+
+const prince = await princeVersion();
+
+if (prince.includes('Prince 16')) {
+  console.log('Rendering PDFs using Prince binary...');
+
+  for (const [lang, filename] of Object.entries(languages)) {
+    exec(`prince --pdf-profile='PDF/UA-1' http://localhost:8080/${lang}/export/ -o src/assets/downloads/${filename}.pdf`, (_error, stdout, _stderr) => {
+      console.log(`Saved ${filename}.pdf.`);
+    });
+  }
+} else {
+  console.log('Rendering PDFs using DocRaptor API...');
+
+  for (const [lang, filename] of Object.entries(languages)) {
+    axios(docraptorConfig(lang))
+    .then(function (response) {
+      fs.writeFile(`./src/assets/downloads/${filename}.pdf`, response.data, "binary", function (
+        writeErr
+      ) {
+        if (writeErr) throw writeErr;
+        console.log(`Saved ${filename}.pdf.`);
+      });
+    })
+    .catch(function (error) {
+      var decoder = new TextDecoder("utf-8");
+      console.error(decoder.decode(error.response.data));
+    });
+  }
+}
