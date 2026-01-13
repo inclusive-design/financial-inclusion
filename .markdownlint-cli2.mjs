dba4c123cfe2c0fe95925ec31a9fc97a7a52df9f
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-01-13 09:17:50 -0400

    chore: ignore collections

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -1,1 +1,6 @@
-export { default } from "@inclusive-design/markdownlint-config";
+import markdownlintConfig from '@inclusive-design/markdownlint-config';
+
+export default {
+	config: markdownlintConfig.config,
+	ignores: ['node_modules', 'src/collections/**', 'CHANGELOG.md'],
+}
